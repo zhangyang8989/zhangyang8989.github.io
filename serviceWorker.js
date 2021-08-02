970d0b1e2ffeb08fa243faf75b649a861bc5180c
21869-zhangyang <yang.zhang12@hand-china.com>
2021-08-02 14:02:22 +0800

    init

--- serviceWorker.js
+++ serviceWorker.js
@@ -21,7 +21,23 @@ this.addEventListener("fetch", function (event) {
   console.log("fetch event");
   event.respondWith(
     /* 在缓存中匹配对应请求资源直接返回 */
-    caches.match(event.request)
+    caches.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found response in cache:', response);
+          return response;
+        }
+        console.log('No response found in cache. About to fetch from network...');
+  
+        return fetch(event.request).then(function(response) {
+          console.log('Response from network is:', response);
+  
+          return response;
+        }).catch(function(error) {
+          console.error('Fetching failed:', error);
+  
+          throw error;
+        });
+      })
   );
 });
 this.addEventListener("message", function (event) {
